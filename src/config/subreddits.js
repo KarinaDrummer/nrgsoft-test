17b4545e1a187ffc423537a9a71cca03ad46d99a
Karina Shedlbauer <k@rina.wtf>
2020-04-02 00:43:00 +0300

    Add subreddits config

---
+++ src/config/subreddits.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    title: 'Frontend',
+    id: 'frontend',
+  },
+  {
+    title: 'ReactJS',
+    id: 'reactjs',
+  },
+  {
+    title: 'VueJS',
+    id: 'vuejs',
+  },
+  {
+    title: 'Angular',
+    id: 'angular',
+  },
+]
