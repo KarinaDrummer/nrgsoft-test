268bb0e7ce0c9860a8b7813e663eb77f9ef12512
Karina Shedlbauer <k@rina.wtf>
2020-04-02 20:19:52 +0300

    Add cors proxy

--- src/api.js
+++ src/api.js
@@ -1,6 +1,10 @@
 import { get } from 'axios'
 
+const corsProxy = 'https://cors-anywhere.herokuapp.com'
+
 export default {
   fetchPosts: async (subredditID) =>
-    await get(`https://reddit.com/r/${subredditID}.json`)
+    await get(
+      `${corsProxy}/https://reddit.com/r/${subredditID}.json`
+    )
 }
